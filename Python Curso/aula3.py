3425b06c7b8b2d2ac2e627f9cb6a363b910665fc
Raphael Belfort <122545860+XraphaelbelfortX@users.noreply.github.com>
2023-08-18 14:50:14 -0300

    Delete Python Curso directory

--- Python Curso/aula3.py
+++
@@ -1,9 +0,0 @@
-# if /    elif    / else
-# se / se não se / se não
-entrada = input('Você quer "entrar" ou "sair"? Digite uma resposta:')
-if entrada == 'entrar':
-    print('Você entrou no sistema')
-elif entrada == 'sair':
-    print('Você saiu do sistema')
-else:
-    print('Você não digitou uma resposta correta')
--- Python Curso/aula6.py
+++
@@ -1,4 +0,0 @@
-nome = 'Raphael'
-preco = 1000.555123
-variavel = '%s, o preço é R$%.2f' %  (nome, preco)
-print(variavel)
